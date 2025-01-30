1a335f7759e18d68485756c940dfd6d7dd510a16
rustiandacode <rustiandazen09@gmail.com>
2025-01-31 00:29:20 +0700

    add global variable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
 export const Home = () => {
+  console.log(import.meta.env.VITE_SOME_KEY); // "123"
+  console.log(import.meta.env.VITE_DB_PASSWORD); // undefined
+
   return <div>Home</div>;
 };
