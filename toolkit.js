c7abdb419a44836e6876aa23888e04b591b60915
rustiandacode <rustiandazen09@gmail.com>
2025-01-31 21:43:40 +0700

    added some action case

--- toolkit.js
+++ toolkit.js
@@ -1,10 +1,16 @@
 import { configureStore, createAction, createReducer } from '@reduxjs/toolkit';
 
 const addToCart = createAction('ADD_TO_CART');
+const deleteCart = createAction('DELETE_CART');
 
 const cartReducer = createReducer([], (builder) => {
   builder.addCase(addToCart, (state, action) => {
     state.push(action.payload);
+  });
+
+  builder.addCase(deleteCart, (state, action) => {
+    state.filter((x) => x.id !== action.payload);
+    // state.pop();
   });
 });
 
@@ -30,4 +36,4 @@ store.subscribe(() => {
 
 store.dispatch(addToCart({ id: 2, qty: 10 }));
 store.dispatch(addToCart({ id: 3, qty: 20 }));
-store.dispatch(loginSession());
+store.dispatch(deleteCart(2));
