2b47a420fcdcc24880eca34549086e7c168f4942
rustiandacode <rustiandazen09@gmail.com>
2025-02-20 18:20:49 +0700

    added close search bar when press the esc button and fixed searcg result desc when there no result we loooking for

--- src/components/molecules/Search.jsx
+++ src/components/molecules/Search.jsx
@@ -3,12 +3,12 @@ import { searchMovie } from '../../services/tmdbapi';
 import { dateConverter } from '../../utils/dateConverter';
 import { findGenre } from '../../utils/findGenreById';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeStatus } from '../../redux/slice/searchSlice';
+import { changeStatus, escPress } from '../../redux/slice/searchSlice';
+import { Link } from 'react-router-dom';
 
 export const Search = () => {
   const [movies, setMovies] = useState([]);
   const [keywords, setKeywords] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
   const baseImgUrl = import.meta.env.VITE_BASE_IMG_URL;
   const [keysPressed, setKeysPressed] = useState({});
   const show = useSelector((state) => state.search.desc);
@@ -16,16 +16,26 @@ export const Search = () => {
 
   useEffect(() => {
     const search = async () => {
-      const query = await searchMovie(keywords);
-      setMovies(query.results);
+      try {
+        const response = await searchMovie(keywords);
+        setMovies(response.results);
+      } catch (error) {
+        console.log('Terjadi kesalahan saat mengambil data:', error.message);
+      }
     };
+
     search();
+
     const handleKeyDown = (event) => {
       setKeysPressed((prev) => ({ ...prev, [event.key.toLowerCase()]: true }));
 
       if (keysPressed['control'] && event.key.toLowerCase() === 'k') {
         event.preventDefault();
         dispatch(changeStatus());
+        setKeywords('');
+      }
+      if (event.key === 'Escape') {
+        dispatch(escPress());
         setKeywords('');
       }
     };
@@ -51,7 +61,7 @@ export const Search = () => {
     <div className={`${show ? 'block' : 'hidden'} absolute top-0 left-0 right-0 bottom-0`}>
       <div className="fixed w-screen h-screen z-50 bg-seasalt dark:bg-jet opacity-95 "></div>
       <div className="flex justify-center">
-        <div className="fixed lg:w-[35%] w-[90%] h-[90%] md:h-[80%] z-50 bg-purewhite dark:bg-onyx md:top-20 top-10 rounded-lg">
+        <div className={`fixed lg:w-[35%] w-[90%] ${movies.length > 0 ? 'h-[90%] md:h-[80%]' : ''}  z-50 bg-purewhite dark:bg-onyx md:top-20 top-10 rounded-lg`}>
           <div className="fixed lg:w-[35%] w-[90%] bg-purewhite dark:bg-onyx flex gap-5 justify-between items-center border-b-1 dark:border-seasalt/10 border-onyx/10 p-5 rounded-t-lg">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="size-6 dark:fill-seasalt fill-jet">
               <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
@@ -68,15 +78,23 @@ export const Search = () => {
             </div>
           </div>
           {!movies.length > 0 && (
-            <div className="pt-20">
-              <p className="text-center dark:text-seasalt text-onyx">No recent searhes</p>
+            <div className="pt-24 pb-8">
+              <p className="text-center dark:text-seasalt text-onyx">{!keywords ? 'No recent searches' : `No results for : ${keywords}`}</p>
             </div>
           )}
           {movies.length > 0 && (
             <div className="pt-16 h-[100%] overflow-scroll">
               {movies.length > 0 &&
                 movies.map((movie, i) => (
-                  <div key={i} className="flex gap-2 border-b-1 dark:border-seasalt/10 border-onyx/10 p-3 text-jet dark:text-seasalt">
+                  <Link
+                    onClick={() => {
+                      dispatch(changeStatus());
+                      setKeywords('');
+                    }}
+                    to={`/movie/${movie.id}`}
+                    key={i}
+                    className="flex gap-2 border-b-1 dark:border-seasalt/10 border-onyx/10 p-3 text-jet dark:text-seasalt"
+                  >
                     <img className="w-20 rounded-lg" src={baseImgUrl + movie.poster_path} alt={movie.title} />
                     <div>
                       <p className="text-lg font-semibold">{movie.title}</p>
@@ -89,7 +107,7 @@ export const Search = () => {
                         ))}
                       </div>
                     </div>
-                  </div>
+                  </Link>
                 ))}
             </div>
           )}
