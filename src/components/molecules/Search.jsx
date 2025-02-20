6c851f57614309ab66814e06705d5e46b87b1334
rustiandacode <rustiandazen09@gmail.com>
2025-02-20 19:25:15 +0700

    fixed scroll on body issue when modal is open

--- src/components/molecules/Search.jsx
+++ src/components/molecules/Search.jsx
@@ -27,6 +27,12 @@ export const Search = () => {
 
     search();
 
+    if (show) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+
     const handleKeyDown = (event) => {
       setKeysPressed((prev) => ({ ...prev, [event.key.toLowerCase()]: true }));
 
@@ -66,13 +72,13 @@ export const Search = () => {
       window.removeEventListener('keyup', handleKeyUp);
       window.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [keywords, keysPressed]);
+  }, [keywords, keysPressed, show]);
 
   return (
     <div className={`${show ? 'block' : 'hidden'} absolute top-0 left-0 right-0 bottom-0`}>
       <div className="fixed w-screen h-screen z-50 bg-seasalt dark:bg-jet opacity-95 "></div>
       <div ref={modalRef} className="flex justify-center">
-        <div className={`fixed lg:w-[35%] w-[90%] ${movies.length > 0 ? 'h-[90%] md:h-[80%]' : ''}  z-50 bg-purewhite dark:bg-onyx md:top-20 top-10 rounded-lg`}>
+        <div className={`fixed lg:w-[35%] w-[90%] ${movies.length > 0 ? 'max-h-[90%] md:max-h-[80%]' : ''} overflow-scroll  z-50 bg-purewhite dark:bg-onyx md:top-20 top-10 rounded-lg`}>
           <div className="fixed lg:w-[35%] w-[90%] bg-purewhite dark:bg-onyx flex gap-5 justify-between items-center border-b-1 dark:border-seasalt/10 border-onyx/10 p-5 rounded-t-lg">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="size-6 dark:fill-seasalt fill-jet">
               <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
@@ -106,11 +112,23 @@ export const Search = () => {
                     key={i}
                     className="flex gap-2 border-b-1 dark:border-seasalt/10 border-onyx/10 p-3 text-jet dark:text-seasalt"
                   >
-                    <img className="w-20 rounded-lg" src={baseImgUrl + movie.poster_path} alt={movie.title} />
+                    {!movie.poster_path && (
+                      <div className="flex justify-center items-center w-20 h-24 bg-seasalt dark:bg-jet rounded-lg ">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 fill-onyx dark:fill-seasalt">
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                          />
+                        </svg>
+                      </div>
+                    )}
+                    {movie.poster_path && <img className="w-20 rounded-lg" src={baseImgUrl + movie.poster_path} alt={movie.title} />}
+
                     <div>
                       <p className="text-lg font-semibold">{movie.title}</p>
                       <p className="text-sm mb-1">{dateConverter(movie.release_date)}</p>
-                      <div className="flex gap-1 flex-wrap ">
+                      <div className="flex gap-1 flex-wrap">
                         {movie.genre_ids.map((id) => (
                           <span key={id} className="p-1 border dark:text-seasalt/50 text-onyx dark:border-seasalt/10 border-onyx/30 rounded text-xs">
                             {id > 0 && findGenre(id)}
