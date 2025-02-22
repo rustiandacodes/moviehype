82fecb92557c106c98cd50e711095e0ec6b38b27
rustiandacode <rustiandazen09@gmail.com>
2025-02-22 19:11:30 +0700

    hidden load more when movies length equal to total results

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -14,12 +14,10 @@ export const Results = () => {
   const [error, setError] = useState();
   const { movie_key } = useParams();
 
-  console.log(movie_key);
-
   useEffect(() => {
     searchMovie(movie_key).then((res) => {
       setMovies(res.results);
-      setTotalPages(res.total_pages);
+      setTotalPages(res);
       console.log(res);
     });
 
@@ -43,12 +41,16 @@ export const Results = () => {
     }
   };
 
+  console.log(movies.length, totalPages.total_results);
+
   return (
     <div className="theme-switch pt-24 min-h-screen">
       <div className=" container mx-auto py-5 px-5 md:px-0">
-        <p>{error}</p>
+        <p>
+          Result for : {error}, {totalPages.total_results}
+        </p>
         <div className="py-5 grid xl:grid-cols-10 md:grid-cols-5 grid-cols-2 gap-4">
-          {isLoading && !error && <CardMovieSkeleton length={20} />}
+          {isLoading && <CardMovieSkeleton length={20} />}
           {!isLoading && movies.length > 0 && movies.map((movie, i) => <CardMovie key={i} title={movie.title} poster={movie.poster_path} rating={movie.vote_average} genre={movie.genre_ids} date={movie.release_date} id={movie.id} />)}
         </div>
         <div
@@ -56,7 +58,7 @@ export const Results = () => {
             showMore();
           }}
         >
-          <div className={`${page > totalPages ? 'hidden' : 'block'} flex justify-center`}>{isLoading && !movies.length > 0 ? <ButtonSkeleton /> : <Button desc="Load More" />}</div>
+          <div className={`${movies.length === totalPages.total_results ? 'hidden' : 'block'} flex justify-center`}>{isLoading && !movies.length > 0 ? <ButtonSkeleton /> : <Button desc="Load More" />}</div>
         </div>
       </div>
     </div>
